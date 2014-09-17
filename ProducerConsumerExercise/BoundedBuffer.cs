bcfcff9e12ee4c067ec7a5b19cf305d5ddb6e348
1NESLEiN <pnielsendk@gmail.com>
2014-09-17 13:36:39 +0200

    no thread safety in the class BoundedBuffer, but the the methods and fields should be implemented

--- ProducerConsumerExercise/BoundedBuffer.cs
+++ ProducerConsumerExercise/BoundedBuffer.cs
@@ -8,6 +8,35 @@ namespace ProducerConsumerExercise
 {
     public class BoundedBuffer
     {
-        
+        private int _capacity;
+        private Object _lockObject = new object();
+        private Queue<int> _bufferData = new Queue<int>();
+
+        public BoundedBuffer(int capacity)
+        {
+            this._capacity = capacity;
+        }
+
+        public void Put(int element)
+        {
+            if (_bufferData.Count != _capacity)
+            {
+                _bufferData.Enqueue(element);
+            }
+        }
+
+        public int Take()
+        {
+            return _bufferData.Dequeue();
+        }
+
+        public bool IsFull()
+        {
+            if (_bufferData.Count != _capacity)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
